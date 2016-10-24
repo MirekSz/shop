f9ea51a6434b42c965448f50459ddabec9e9f54a
MirekSz <miro1994>
2016-10-24 18:22:27 +0200

    bootstrap

---
+++ shop/src/main/java/pl/altkom/shop/web/WebBootstrap.java
@@ -0,0 +1,41 @@
+package pl.altkom.shop.web;
+
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import pl.altkom.shop.CoreConfig;
+
+public class WebBootstrap implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(CoreConfig.class);
+		ctx.setServletContext(container);
+
+		ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
+
+		forceUTF8(container);
+	}
+
+	private void forceUTF8(ServletContext container) {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+
+		container.addFilter("characterEncoding", characterEncodingFilter).addMappingForUrlPatterns(
+				EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), true, "/*");
+	}
+}
