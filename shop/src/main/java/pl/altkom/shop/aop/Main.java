633e8d6ce2775cf6e3aa96e3dbe803aeabe2c2c2
MirekSz <miro1994>
2016-10-26 08:47:07 +0200

    s

---
+++ shop/src/main/java/pl/altkom/shop/aop/Main.java
@@ -0,0 +1,34 @@
+package pl.altkom.shop.aop;
+
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.util.Base64Utils;
+import org.springframework.web.client.RestTemplate;
+
+import pl.altkom.shop.model.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		HttpHeaders headers = new HttpHeaders();
+		String string = "Basic " + Base64Utils.encodeToString("user:user".getBytes(Charset.forName("UTF-8")));
+		System.out.println(string);
+		headers.add("Authorization", string);
+		RestTemplate restTemplate = new RestTemplate();
+		((SimpleClientHttpRequestFactory) restTemplate.getRequestFactory()).setReadTimeout(1000 * 30);
+		((SimpleClientHttpRequestFactory) restTemplate.getRequestFactory()).setConnectTimeout(1000 * 30);
+
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<Product[]> exchange = restTemplate.exchange("http://localhost:8080/shop/rest/product/list",
+				HttpMethod.GET, entity, Product[].class);
+		exchange = restTemplate.exchange("http://localhost:8080/shop/rest/product/list", HttpMethod.GET, entity,
+				Product[].class);
+		System.out.println(exchange.getBody()[0].getName());
+		System.out.println(exchange.getHeaders());
+	}
+}
--- shop/src/main/java/pl/altkom/shop/controller/RestProductController.java
+++ shop/src/main/java/pl/altkom/shop/controller/RestProductController.java
@@ -1,8 +1,10 @@
 package pl.altkom.shop.controller;
 
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +21,13 @@ public class RestProductController {
 	ProductRepo repo;
 
 	@RequestMapping("/list")
-	public List<Product> list(Model model) throws Exception {
+	public List<Product> list(Model model, HttpServletRequest reg) throws Exception {
+		Enumeration<String> headerNames = reg.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String nextElement = headerNames.nextElement();
+			String header = reg.getHeader(nextElement);
+			System.out.println(nextElement + " " + header);
+		}
 		return repo.getAll();
 	}
 
