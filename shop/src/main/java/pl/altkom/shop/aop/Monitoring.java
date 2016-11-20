8970bdc5fe130805e984727d3c91a4be2899b7ab
MirekSz <miro1994>
2016-11-20 13:37:30 +0100

    zero

--- shop/src/main/java/pl/altkom/shop/aop/Monitoring.java
+++
@@ -1,7 +0,0 @@
-package pl.altkom.shop.aop;
-
-@java.lang.annotation.Target(value = { java.lang.annotation.ElementType.TYPE })
-@java.lang.annotation.Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
-public @interface Monitoring {
-	long maxTime() default 100L;
-}
--- shop/src/main/java/pl/altkom/shop/service/DocumentRequest.java
+++
@@ -1,8 +0,0 @@
-package pl.altkom.shop.service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class DocumentRequest {
-	public List<Item> items = new ArrayList();
-}
