761a0e4ad25e89413c4995a88105fd7ef8adf6ed
MirekSz <miro1994>
2016-11-22 22:02:08 +0100

    docrequest

---
+++ shop/src/main/java/pl/altkom/shop/service/SaleDocumentService.java
@@ -0,0 +1,31 @@
+package pl.altkom.shop.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import pl.altkom.shop.model.Product;
+import pl.altkom.shop.model.SaleDocument;
+import pl.altkom.shop.model.SaleDocumentItem;
+
+//@Service
+public class SaleDocumentService {
+	@Inject
+	EntityManager em;
+
+	public void insert(DocumentRequest documentRequest) {
+		SaleDocument saleDocument = new SaleDocument();
+		em.persist(saleDocument);
+		List<Item> items = documentRequest.items;
+		for (Item item : items) {
+			SaleDocumentItem saleDocumentItem = new SaleDocumentItem();
+			Product product = em.find(Product.class, item.idProduct);
+			saleDocumentItem.setProduct(product);
+			saleDocumentItem.setQuantity(item.quantity);
+			saleDocumentItem.setSaleDocument(saleDocument);
+			product.setQuantity(product.getQuantity() - item.quantity);
+		}
+	}
+
+}
