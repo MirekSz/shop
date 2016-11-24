b60ab3155c3bec17303506cab86f48bc613bdd02
MirekSz <m.szajowski@gmail.com>
2016-11-24 18:10:54 +0100

    without sec

--- shop/src/main/java/pl/altkom/shop/www/SecurityConfig.java
+++ shop/src/main/java/pl/altkom/shop/www/SecurityConfig.java
@@ -2,9 +2,13 @@ package pl.altkom.shop.www;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+
+import pl.altkom.shop.Profiles;
 
 @Configuration
 // @EnableWebSecurity
 @ImportResource("classpath:security.xml")
+@Profile(Profiles.WEB)
 public class SecurityConfig {
 }
